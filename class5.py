1a066d1ff84a1a4dfbdbf3ca57bee4ad9c2b03af
Samson Adejoro <samsonadejoro@gmail.com>
2019-12-01 13:03:20 +0100

    Update class5.py

--- class5.py
+++ class5.py
@@ -3,21 +3,21 @@ object from the class—for example, c = Converter(9,'inches'). The possible uni
 inches, feet, yards, miles, kilometers, meters, centimeters, and millimeters. For each of these
 units there should be a method that returns the length converted into those units.
 For example, using the Converter object created above, the user could call c.feet() and should get
-108 as the result.'''
+0.75 as the result.'''
 
 class Converter:
     def __init__(self,length,unit):
         self.length = length
-        self.unit = unit.lower()
+        self.unit = unit
 
     def inches(self):    
-        conv_feet = self.length/12
-        conv_yards = self.length/36
-        conv_miles = self.length/63360
-        conv_kmeters = self.length/39370.079
-        conv_meters = self.length/39.37
-        conv_centimeters = self.length*2.54
-        conv_millimeters = self.length/25.4
+        conv_feet = self.length*12
+        conv_yards = self.length*36
+        conv_miles = self.length*63360
+        conv_kmeters = self.length*39370.079
+        conv_meters = self.length*39.37
+        conv_centimeters = self.length/2.54
+        conv_millimeters = self.length*25.4
 
         conv_list = [conv_feet,conv_yards,conv_miles,conv_kmeters,conv_meters,conv_centimeters,conv_millimeters]
         if self.unit == 'feet':
@@ -37,13 +37,13 @@ class Converter:
             
       
     def feet(self):
-        conv_inches = self.length*12
-        conv_yards = self.length/3
-        conv_miles = self.length/5280
-        conv_kmeters = self.length/3280.84
-        conv_meters = self.length/3.281
-        conv_centimeters = self.length*30.48
-        conv_millimeters = self.length*304.8
+        conv_inches = self.length/12
+        conv_yards = self.length*3
+        conv_miles = self.length*5280
+        conv_kmeters = self.length*3280.84
+        conv_meters = self.length*3.281
+        conv_centimeters = self.length/30.48
+        conv_millimeters = self.length/304.8
 
         conv_list = [conv_inches,conv_yards,conv_miles,conv_kmeters,conv_meters,conv_centimeters,conv_millimeters]
         if self.unit == 'inches':
@@ -62,13 +62,13 @@ class Converter:
             return conv_list[6]
 
     def yards(self):
-        conv_feet = self.length*3
-        conv_inches = self.length*36
-        conv_miles = self.length/1760
-        conv_kmeters = self.length/1093.613
-        conv_meters = self.length/1.094
-        conv_centimeters = self.length/91.44
-        conv_millimeters = self.length/914.4
+        conv_feet = self.length/3
+        conv_inches = self.length/36
+        conv_miles = self.length*1760
+        conv_kmeters = self.length*1093.613
+        conv_meters = self.length*1.094
+        conv_centimeters = self.length*91.44
+        conv_millimeters = self.length*914.4
 
         conv_list = [conv_feet,conv_inches,conv_miles,conv_kmeters,conv_meters,conv_centimeters,conv_millimeters]
         if self.unit == 'feet':
@@ -88,13 +88,13 @@ class Converter:
         
 
     def miles(self):
-        conv_feet = self.length*5280
-        conv_yards = self.length*1760
-        conv_inches = self.length*63360
-        conv_kmeters = self.length*1.609
-        conv_meters = self.length*1609.344
-        conv_centimeters = self.length*160934.4
-        conv_millimeters = self.length/1.609e+6
+        conv_feet = self.length/5280
+        conv_yards = self.length/1760
+        conv_inches = self.length/63360
+        conv_kmeters = self.length/1.609
+        conv_meters = self.length/1609.344
+        conv_centimeters = self.length/160934.4
+        conv_millimeters = self.length*1.609e+6
 
         conv_list = [conv_feet,conv_inches,conv_yards,conv_kmeters,conv_meters,conv_centimeters,conv_millimeters]
         if self.unit == 'feet':
@@ -114,13 +114,13 @@ class Converter:
 
 
     def kilometers(self):
-        conv_feet = self.length*3280.84
-        conv_yards = self.length*1093.613
-        conv_miles = self.length/1.609
-        conv_meters = self.length*1000
-        conv_centimeters = self.length*100000
-        conv_millimeters = self.length*1e+6
-        conv_inches = self.length*39370.079
+        conv_feet = self.length/3280.84
+        conv_yards = self.length/1093.613
+        conv_miles = self.length*1.609
+        conv_meters = self.length/1000
+        conv_centimeters = self.length/100000
+        conv_millimeters = self.length/1e+6
+        conv_inches = self.length/39370.079
 
 
         conv_list = [conv_feet,conv_inches,conv_miles,conv_yards,conv_meters,conv_centimeters,conv_millimeters]
@@ -141,13 +141,13 @@ class Converter:
 
 
     def meters(self):
-        conv_feet = self.length*3.281
-        conv_yards = self.length*1.094
-        conv_miles = self.length/1609.344
-        conv_kmeters = self.length*1000
-        conv_inches = self.length*39.37
-        conv_centimeters = self.length*100
-        conv_millimeters = self.length*1000
+        conv_feet = self.length/3.281
+        conv_yards = self.length/1.094
+        conv_miles = self.length*1609.344
+        conv_kmeters = self.length/1000
+        conv_inches = self.length/39.37
+        conv_centimeters = self.length/100
+        conv_millimeters = self.length/1000
 
         conv_list = [conv_feet,conv_inches,conv_miles,conv_yards,conv_kmeters,conv_centimeters,conv_millimeters]
         if self.unit == 'feet':
@@ -167,13 +167,13 @@ class Converter:
 
 
     def centimeters(self):
-        conv_feet = self.length/30.48
-        conv_yards = self.length/91.44
-        conv_miles = self.length/160934.4
-        conv_inches = self.length/2.54
-        conv_meters = self.length/100
-        conv_kmeters = self.length/100000
-        conv_millimeters = self.length*10
+        conv_feet = self.length*30.48
+        conv_yards = self.length*91.44
+        conv_miles = self.length*160934.4
+        conv_inches = self.length*2.54
+        conv_meters = self.length*100
+        conv_kmeters = self.length*100000
+        conv_millimeters = self.length/10
 
         conv_list = [conv_feet,conv_inches,conv_miles,conv_yards,conv_kmeters,conv_meters,conv_millimeters]
         if self.unit == 'feet':
@@ -193,13 +193,13 @@ class Converter:
         
     
     def millimeters(self):
-        conv_feet = self.length/304.8
-        conv_yards = self.length/914.4
-        conv_miles = self.length/1.609e+6
-        conv_kmeters = self.length/1e+6
-        conv_inches = self.length/25.4
-        conv_centimeters = self.length/10
-        conv_meters = self.length/1000
+        conv_feet = self.length*304.8
+        conv_yards = self.length*914.4
+        conv_miles = self.length*1.609e+6
+        conv_kmeters = self.length*1e+6
+        conv_inches = self.length*25.4
+        conv_centimeters = self.length*10
+        conv_meters = self.length*1000
 
         conv_list = [conv_feet,conv_inches,conv_miles,conv_yards,conv_kmeters,conv_meters,conv_centimeters]
         if self.unit == 'feet':
@@ -217,7 +217,9 @@ class Converter:
         elif self.unit == 'centimeters':
             return conv_list[6]
 
-a = Converter(9,'inches')
+user_input = eval(input('Enter an integer: '))
+unit = input('Enter a unit ')
+a = Converter(user_input,unit)
 print(a.feet())
 
         
